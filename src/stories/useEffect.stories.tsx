2670d54a7e7780c9a952004d0a44967d0f07afa3
igorlitvintsev <litvincevi@mail.ru>
2021-04-19 23:31:43 +0300

    21.useEffect  setTimeout  setInterval

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -37,3 +37,23 @@ export const SimpleExample = () => {
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 }
+
+export const SetTimeoutExample = () => {
+
+    const [fake, setFake] = useState(1);
+    const [counter, setCounter] = useState(1);
+    console.log("SimpleExample")
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+
+    return <>
+        Hello, counter: {counter} fake: {fake}
+        {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+        {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+    </>
+}
